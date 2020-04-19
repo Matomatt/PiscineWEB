2e6cd5eb140723e578209ed82909703ded1629fe
Matth <Matth@DESKTOP-A1MN6A3>
2020-04-19 18:29:33 +0200

    Fix

--- Acheteur/tableau_de_bord.php
+++ Acheteur/tableau_de_bord.php
@@ -1,7 +1,7 @@
-<div class="wishlist col-lg-4"  style=float:none >
-<div class="col-lg-5 col-xs-5">
+
+<div class="wishlist col-lg-8">
     <h3>Ma wishlist</h3>
-    <table type="table">
+    <table>
 
 <!----------------------------------->
         <?php
@@ -30,40 +30,26 @@
 
                 while($row = $result->fetch_assoc()) 
                 {   
-                    $queryQT = "SELECT Quantite FROM paniers WHERE Id_Item =" . $row["ID"] . " AND ID_Acheteur=".$ID_Acheteur.";";
-                    $resultQT = mysqli_query($db_handle, $queryQT);
-                    $Quantite = 1;
-                    if ($resultQT)
-                    {
-                        $Quantite = $resultQT->fetch_assoc()["Quantite"];
-                    }
                     /*on récupère les données de la table medias*/
                     $img = mysqli_query($db_handle, "SELECT File FROM medias WHERE ID_Item=" . $row["ID"] . " AND indx = 0;")->fetch_assoc() ["File"];
                     echo '<tr style="text-align: justify;">
                             <td><img style="max-width: 10em; max-height: 10em" src="../UploadedContent/'. (($img!="") ? $img : 'blank.png') . '" ></td>';
 
-                    echo '<td style="padding: 5px; padding-right: 10px;">
-                            <p class="nomproduit"><strong>Nom produit : </strong>'.$row["Nom"].'</p>
-                            <p class="description " style="width:150px;">'.$row["Description"].'</p>
-                         </td>
-                         <td>
-                            <p class="prix">Prix : '.$row["Prix"].'€</p>
+                    echo '<td style="padding-left: 1em;">
+                            <strong>'.$row["Nom"].'</strong><br>
+                            Prix : '.$row["Prix"].'€
                         </td>
                         <td>
-                            <button style="margin-left:15px;"><a href="../Produit/supprimerDeWishlists.php?id1='.$row["ID"].'&id2='.$ID_Acheteur.'">Supprimer</a></button>
+                            <button style="margin-left: 1em;"><a href="../Produit/supprimerDeWishlists.php?id1='.$row["ID"].'&id2='.$ID_Acheteur.'">Supprimer</a></button>
                         </td>
-                            </div>
-                         </td>
                    </tr>';
                 }
             ?>
-
-<!----------------------------------->
-       
         </table>
 
     </div>
 
+<!--  
 </div>
 <div class="enchere col-lg-4" >
     <div class="col-lg-2 col-xs-5">
@@ -74,3 +60,4 @@
 
     </div>
 </div>
+-->
